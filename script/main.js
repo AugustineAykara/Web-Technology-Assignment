adb0c83bcea7a7ad51c989fd23e46e951d13a496
augustineaykara <augustineaykara@gmail.com>
2020-04-06 01:34:10 +0530

    upload via git

--- script/main.js
+++ script/main.js
@@ -6,21 +6,34 @@ collegeName = document.form.collegeName.value
 course = document.form.course.value
 var password = document.form.password
 var confirmPassword = document.form.confirmPassword
+var message = document.getElementById('message')
+
+function passwordStrength(){
+    password = password.value
+    confirmPassword = confirmPassword.value
+    if(password.length <= 6){
+        message.innerHTML = "Weak Password"        
+    }
+    else{
+        message.set = "Strong Password"
+    }
+}
+
+function passwordValidation() {
+    if (password != confirmPassword) {
+        alert("Password not matching. Try again")
+        password.value = ''
+        confirmPassword.value = ''
+    }
+    else{
+        true
+    }
+}
 
 function validateForm() {
     // if(name == '' || username == '' || email == '' || mobile == '' || collegeName == '' || course =='' || password =='' || confirmPassword == ''){
     //     alert("Please complete the form")
     //     return false
     // }
-    if (password.value != confirmPassword.value) {
-
-        alert("Password not matching. Try again")
-        password.value = ''
-        confirmPassword.value = ''
-        return false
-    }
-    else{
-        alert("success")
-        return true
-    }
+    passwordValidation()
 }
