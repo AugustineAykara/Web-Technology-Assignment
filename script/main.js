857d023cfb2bba9130e826a2e60e8419ecc36844
augustineaykara <augustineaykara@gmail.com>
2020-04-06 00:45:46 +0530

    upload via git

--- script/main.js
+++ script/main.js
@@ -1,1 +1,26 @@
-console.log("test");
+name = document.form.name.value
+username = document.form.username.value
+email = document.form.email.value
+mobile = document.form.mobile.value
+collegeName = document.form.collegeName.value
+course = document.form.course.value
+var password = document.form.password
+var confirmPassword = document.form.confirmPassword
+
+function validateForm() {
+    // if(name == '' || username == '' || email == '' || mobile == '' || collegeName == '' || course =='' || password =='' || confirmPassword == ''){
+    //     alert("Please complete the form")
+    //     return false
+    // }
+    if (password.value != confirmPassword.value) {
+
+        alert("Password not matching. Try again")
+        password.value = ''
+        confirmPassword.value = ''
+        return false
+    }
+    else{
+        alert("success")
+        return true
+    }
+}
