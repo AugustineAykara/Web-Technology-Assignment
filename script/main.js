7dfa276e9765ac86a7aea9e4866958153a16de7e
augustineaykara <augustineaykara@gmail.com>
2020-04-07 01:11:03 +0530

    upload via git

--- script/main.js
+++ script/main.js
@@ -1,39 +1,47 @@
-name = document.form.name.value
-username = document.form.username.value
-email = document.form.email.value
-mobile = document.form.mobile.value
-collegeName = document.form.collegeName.value
-course = document.form.course.value
-var password = document.form.password
-var confirmPassword = document.form.confirmPassword
-var message = document.getElementById('message')
 
-function passwordStrength(){
-    password = password.value
-    confirmPassword = confirmPassword.value
-    if(password.length <= 6){
-        message.innerHTML = "Weak Password"        
+function passwordStrength() {
+    message = document.getElementById('message')
+    password = document.form.password.value
+    confirmPassword = document.form.confirmPassword.value
+    if (password.length <= 6) {
+        message.innerHTML = "Weak Password"
+        message.style.color = "red"
     }
-    else{
-        message.set = "Strong Password"
+    else {
+        message.innerHTML = "Strong Password"
+        message.style.color = "green"
     }
 }
 
-function passwordValidation() {
-    if (password != confirmPassword) {
-        alert("Password not matching. Try again")
-        password.value = ''
-        confirmPassword.value = ''
-    }
-    else{
-        true
-    }
-}
 
 function validateForm() {
-    // if(name == '' || username == '' || email == '' || mobile == '' || collegeName == '' || course =='' || password =='' || confirmPassword == ''){
-    //     alert("Please complete the form")
-    //     return false
-    // }
-    passwordValidation()
+    name = document.form.name.value
+    username = document.form.username.value
+    email = document.form.email.value
+    mobile = document.form.mobile.value
+    collegeName = document.form.collegeName.value
+    course = document.form.course.value
+    password = document.form.password.value
+    confirmPassword = document.form.confirmPassword.value
+
+    if(name == '' || username == '' || email == '' || mobile == '' || collegeName == '' || course =='' || password =='' || confirmPassword == ''){
+        alert("Please complete the form")
+        return false
+    }
+    else if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+        alert("Please enter a valid email-ID")
+        return false
+    }
+    else if(mobile.length < 10){
+        alert("Please enter a valid mobile number")
+        return false
+    }
+    else if (password != confirmPassword) {
+        alert("Password not matching. Try again")
+        return false
+    }
+    else{
+        alert("Your form has been submitted successfully")
+        return true
+    }
 }
