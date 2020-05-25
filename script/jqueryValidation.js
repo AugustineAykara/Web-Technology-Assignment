30565d73220998643b562b762c965bc9a20a1db9
augustineaykara <augustineaykara@gmail.com>
2020-05-25 10:07:59 +0530

    upload via git

--- script/jqueryValidation.js
+++ script/jqueryValidation.js
@@ -7,11 +7,30 @@ $(function(){
         var collegeName = $("input[name='collegeName']").val();        
         var password = $("input[name='password']").val();        
         var confirmPassword = $("input[name='confirmPassword']").val();        
-
-        if(name == '' || username == '' || email == '' || mobile == '' || collegeName == '' || course =='' || password =='' || confirmPassword == ''){
+        console.log(password + " " + confirmPassword)
+        if(name == '' || username == '' || email == '' || mobile == '' || collegeName == '' || password =='' || confirmPassword == ''){
             alert("Please complete the form")
             return false
         }
-       
+        else if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+            alert("Please enter a valid email-ID")
+            return false
+        }
+        else if(mobile.length < 10){
+            alert("Please enter a valid mobile number")
+            return(false)
+        }
+        else if (password.length < 8) {
+            alert("Please enter a strong password(min 8 char)")
+            return(false)
+        }
+        else if(password != confirmPassword) {
+            alert("Password not matching. Try again")
+            return(false)
+        }
+        else{
+            alert("Your form has been submitted successfully")
+            return true
+        }           
     });
 });
